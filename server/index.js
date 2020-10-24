5cabe66ab3e09fd074cf381360065065aa471d96
aftabumer <innocentboy0300@gmail.com>
2020-10-24 23:35:13 +0500

    -create custom server on express.

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const next = require("next");
+const bodyParser = require("body-parser");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(bodyParser.json());
+
+  server.get("/api/v1/movies", (req, res) => {
+    return res.json({ message: "Helloo world" });
+  });
+  server.post("/api/v1/movies", (req, res) => {
+    const movie = req.body;
+    console.log(JSON.stringify(movie));
+    return res.json({ ...movie, createdTime: "today", author: "Aftab" });
+  });
+  server.patch("/api/v1/movies/:id", (req, res) => {
+    const { id } = req.params;
+    return res.json({ message: `Updating post of id: ${id}` });
+  });
+  server.delete("/api/v1/movies/:id", (req, res) => {
+    const { id } = req.params;
+    return res.json({ message: `Delete post of id:  ${id}` });
+  });
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.get("/faq", (req, res) => {
+    res.send(`
+      <html>
+            <head></head>
+            <body>
+                <h1>html dirext render on example page</h1>
+            </body>
+        </html>
+    `);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
